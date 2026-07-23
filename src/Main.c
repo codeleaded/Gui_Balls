c190b076eb54c8a81bd58e4ea16a3bd90b340fce
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:40:01 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,6 @@
-// #include "C:/Wichtig/System/Static/Library/WindowEngine1.0.h"
+// #include "C:/Wichtig/System/Static/Library/WindowEngine.h"
 // #include "C:/Wichtig/System/Static/Library/Random.h"
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/Random.h"
 #include "/home/codeleaded/System/Static/Library/TransformedView.h"
 
